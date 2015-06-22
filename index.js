dd9c4ba5d43a05d1b115ad337b94cf9a76cde970
Kevin Lanni <therealklanni@gmail.com>
2015-06-21 22:45:10 -0700

    match case

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 'use strict';
+
+function setCase(str, strCase) {
+	return str['to' + strCase + 'Case']();
+}
+
 module.exports = function (str, plural, count) {
 	if (typeof plural === 'number') {
 		count = plural;
-		plural = str + 's';
+
+		var plur = 's';
+		// get the tail of the string to check if the case should be upper or lower
+		var strCase = str.slice(1) === str.slice(1).toUpperCase() ? 'Upper' : 'Lower';
+		plural = str + setCase(plur, strCase);
 	}
 
 	return count === 1 ? str : plural;
--- test.js
+++ test.js
@@ -2,10 +2,11 @@
 var test = require('ava');
 var plur = require('./');
 
-test(function (t) {
+test('Plur', function (t) {
 	t.assert(plur('unicorn', 0) === 'unicorns');
 	t.assert(plur('unicorn', 1) === 'unicorn');
 	t.assert(plur('unicorn', 2) === 'unicorns');
+	t.assert(plur('UNICORN', 2) === 'UNICORNS');
 	t.assert(plur('unicorn', 'horse', 0) === 'horse');
 	t.assert(plur('unicorn', 'horse', 1) === 'unicorn');
 	t.assert(plur('unicorn', 'horse', 2) === 'horse');
