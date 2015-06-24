49af1db51fb0bd9a2fb4cf68618605b5047c68af
Kevin Lanni <therealklanni@gmail.com>
2015-06-23 20:18:37 -0700

    simplified the logic
    It made more sense to use `.replace()`, which made the case-matching
    seem like too much complexity, so it was removed. K.I.S.S.!

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 'use strict';
-
-function setCase(str, strCase) {
-	return str['to' + strCase + 'Case']();
-}
-
 module.exports = function (str, plural, count) {
 	if (typeof plural === 'number') {
 		count = plural;
-
-		var plur = 's';
-		// get the tail of the string to check if the case should be upper or lower
-		var strCase = str.slice(1) === str.slice(1).toUpperCase() ? 'Upper' : 'Lower';
-		var end = str.slice(-2);
-
-		if (/(s|x|z|ch|sh)$/i.test(end)) {
-			plur = 'es';
-		} else if (/y/i.test(end)) {
-			str = str.slice(0, -1);
-			plur = 'ies';
-		}
-
-		plural = str + setCase(plur, strCase);
+		plural = str.replace(/(s|x|z|ch|sh)$/i, '$1e').replace(/y$/i, 'ie') + 's';
 	}
 
 	return count === 1 ? str : plural;
