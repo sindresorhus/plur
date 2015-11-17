31fb61a3f57fd615655c9b4a0de527cda063587d
Kevin Lanni <therealklanni@gmail.com>
2015-11-17 11:26:20 -0700

    fix -y word pluralization

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function (str, plural, count) {
 	} else if (typeof plural === 'number') {
 		count = plural;
 
-		plural = (str.replace(/(?:s|x|z|ch|sh)$/i, '$&e').replace(/y$/i, 'ie') + 's')
+		plural = (str.replace(/(?:s|x|z|ch|sh)$/i, '$&e').replace(/([^aeiou])y$/i, '$1ie') + 's')
 			.replace(/i?e?s$/i, function (m) {
 				var isTailLowerCase = str.slice(-1) === str.slice(-1).toLowerCase();
 				return isTailLowerCase ? m.toLowerCase() : m.toUpperCase();
