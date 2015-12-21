1346a8654aadd5c49506ad3ce28accd9998b1335
Alex Meah <alexmeahdotcom@gmail.com>
2015-12-21 14:08:16 +0000

    Fix: Make irregular plurals respect count
    Irregular are always pluralized at the moment

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 var irregularPlurals = require('irregular-plurals');
 
 module.exports = function (str, plural, count) {
+	if (typeof plural === 'number') {
+		count = plural;
+	}
+
 	if (str in irregularPlurals) {
 		plural = irregularPlurals[str];
-	} else if (typeof plural === 'number') {
-		count = plural;
-
+	} else if (typeof plural !== 'string') {
 		plural = (str.replace(/(?:s|x|z|ch|sh)$/i, '$&e').replace(/([^aeiou])y$/i, '$1ie') + 's')
 			.replace(/i?e?s$/i, function (m) {
 				var isTailLowerCase = str.slice(-1) === str.slice(-1).toLowerCase();
