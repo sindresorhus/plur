3ed4304737ab90b816fb88dcc8a98ac4b7d54dda
Sindre Sorhus <sindresorhus@gmail.com>
2018-04-08 16:40:15 +0700

    Require Node.js 6

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 const irregularPlurals = require('irregular-plurals');
 
-module.exports = function (str, plural, count) {
+module.exports = (word, plural, count) => {
 	if (typeof plural === 'number') {
 		count = plural;
 	}
 
-	if (str in irregularPlurals) {
-		plural = irregularPlurals[str];
+	if (irregularPlurals.has(word)) {
+		plural = irregularPlurals.get(word);
 	} else if (typeof plural !== 'string') {
-		plural = (str.replace(/(?:s|x|z|ch|sh)$/i, '$&e').replace(/([^aeiou])y$/i, '$1ie') + 's')
+		plural = (word.replace(/(?:s|x|z|ch|sh)$/i, '$&e').replace(/([^aeiou])y$/i, '$1ie') + 's')
 			.replace(/i?e?s$/i, m => {
-				const isTailLowerCase = str.slice(-1) === str.slice(-1).toLowerCase();
+				const isTailLowerCase = word.slice(-1) === word.slice(-1).toLowerCase();
 				return isTailLowerCase ? m.toLowerCase() : m.toUpperCase();
 			});
 	}
 
-	return Math.abs(count) === 1 ? str : plural;
+	return Math.abs(count) === 1 ? word : plural;
 };
